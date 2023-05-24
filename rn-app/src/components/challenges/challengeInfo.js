bbeda5dd7a22faffd823a6b078f6df06860f63c4
JustinChong0 <justin.r.chong.24@dartmouth.edu>
2023-05-23 23:35:10 -0400

    back nav for challenge info and screen display props settings

--- rn-app/src/components/challenges/challengeInfo.js
+++ rn-app/src/components/challenges/challengeInfo.js
@@ -1,16 +1,39 @@
 import React from 'react';
 import {
-    StyleSheet, View, Text, Image,
+    StyleSheet, View, Text, Image, TouchableOpacity,
 } from 'react-native';
 
 const ChallengeInfo = ({navigation}) => {
     return (
-        <View>
+        <View style={styles.screen}>
+            <TouchableOpacity onPress={() => {navigation.goBack()}}>
+                <Image 
+                    style={styles.backButton}
+                    source={require('./../../../assets/icons/back-button.png')}
+                />
+            </TouchableOpacity>
+
+            <Text>
+                Dye Your Hair Pink
+            </Text>
             <Text
                 onPress={() => {navigation.navigate('Video Album')}}
             >Challenge Info</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    screen: {
+        paddingHorizontal: 25,
+        paddingTop: 55,
+        backgroundColor: 'blue',
+    },
+
+    backButton: {
+        width: 20,
+        height: 40,
+    }
+})
 
 export default ChallengeInfo;
