4cf4480a8e4387e65d1edf919172d37641fcd2ea
JustinChong0 <justin.r.chong.24@dartmouth.edu>
2023-05-25 15:41:09 -0400

    added stack navgiator for History Screen

--- rn-app/src/features/history/history.jsx
+++ rn-app/src/features/history/history.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import {
-    StyleSheet, View, Text, Image,
+    StyleSheet, View, Text, Image, FlatList, SafeAreaView, Pressable, RefreshControl, TouchableOpacity
 } from 'react-native';
-import HistoryItem from './historyItem'
 
-const History = (props) => {
+const History = ({navigation}) => {
     return (
-        <View>
+        <View style={styles.screen}>
             <Text>History Page</Text>
         </View>
     )
 }
 
+const styles = StyleSheet.create({
+    screen: {
+        paddingHorizontal: 30,
+        paddingTop: 55,
+    }
+})
 export default History;
