c709309d32865fa3cdbd9254d401615843976309
JustinChong0 <justin.r.chong.24@dartmouth.edu>
2023-05-25 14:43:23 -0400

    basic display of saved data from hardwired object list

--- rn-app/src/features/saved/saved.jsx
+++ rn-app/src/features/saved/saved.jsx
@@ -1,25 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-    StyleSheet, View, Text, Image,
+    StyleSheet, View, Text, Image, FlatList, SafeAreaView, Pressable
 } from 'react-native';
 import SavedHeader from './../../../assets/images/saved-header.png';
+import Bookmark from '../../../assets/icons/bookmark.png';
+import BookmarkFilled from '../../../assets/icons/bookmark-filled.png';
 
 const Saved = ({navigation}) => {
+    const savedData = [
+        {
+            id: '1',
+            title: 'DYE YOUR HAIR PINK',
+            expiresAt: Date.now + (7 * 24 * 60 * 60 * 1000),
+            points: 75,
+        },
+
+        {
+            id: '2',
+            title: 'GET A DRAGON TATTOO',
+            expiresAt: Date.now + (7 * 24 * 60 * 60 * 1000),
+            points: 200,
+        },
+
+    ];
+
+    const SavedItem = ({ title, expiresAt, points }) => {
+        const [challengedSaved, setChallengeSaved] = useState(true);
+
+        return (
+            <View style={styles.savedItem}>
+                <Text style={styles.title}>{title}</Text>
+                <View>
+                    <Text style={styles.expiresAt}>Expires in 3 hours</Text>
+                    <View>
+                        <Text style={styles.points}>{points}</Text>
+                        <Pressable onPress={() => {setChallengeSaved(!challengedSaved)}}>
+                            <Image
+                                style={styles.bookmark}
+                                source={challengedSaved ? BookmarkFilled : Bookmark}
+                            />
+                        </Pressable>
+                    </View>
+                </View>
+            </View>
+        )
+    };
+    
     return (
         <View style={styles.screen}>
             <View style={styles.pointsContainer}>
-                <Text style={styles.points}>50 pts</Text>
+                <Text style={styles.currentPoints}>50 pts</Text>
             </View>
             <Image 
                 source={SavedHeader}
                 style={styles.savedHeader}
             />
-            <Text>
-                Saved Challenges Page
-            </Text>
-            <Text onPress={() => {navigation.navigate('Challenge Info')}}>
+
+            <SafeAreaView style={styles.savedContainer}>
+                <FlatList
+                    style={styles.savedList}
+                    data={savedData}
+                    renderItem={({item}) => 
+                        <SavedItem
+                            title={item.title}
+                            expiresAt={item.expiresAt}
+                            points={item.points}
+                        />
+                    }
+                    keyExtractor={item => item.id}
+                />
+            </SafeAreaView>
+            
+            {/* <Text onPress={() => {navigation.navigate('Challenge Info')}}>
                 Click on a saved challenge to see info about it
-            </Text>
+            </Text> */}
         </View>
     )
 }
@@ -30,7 +84,7 @@ const styles = StyleSheet.create({
         paddingTop: 55,
     },
 
-    points: {
+    currentPoints: {
         color: '#99F9FF',
         borderWidth: 1,
         borderColor: '#7BF7FF',
@@ -44,6 +98,34 @@ const styles = StyleSheet.create({
         width: 150,
         height: 52,
         marginTop: 10,
+    },
+
+    savedContainer: {
+    },
+
+    savedList: {
+
+    },
+
+    savedItem: {
+
+    },
+
+    title: {
+        color: '#ffffff',
+    },
+
+    expiresAt: {
+        color: '#ffffff'
+    },
+
+    points: {
+        color: '#ffffff',
+    },
+
+    bookmark: {
+        width: 30,
+        height: 30,
     }
 })
 
