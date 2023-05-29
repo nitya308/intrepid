b3fa00b34a532a749b71c5f8ba5c9b1f89c085bd
JustinChong0 <justin.r.chong.24@dartmouth.edu>
2023-05-29 02:06:55 -0400

    completed entry page

--- rn-app/src/entry/entry.jsx
+++ rn-app/src/entry/entry.jsx
@@ -2,31 +2,79 @@ import React, { useEffect, useState } from 'react';
 import {
     StyleSheet, View, Text, Image, TouchableOpacity, Button
 } from 'react-native';
+import EntryLogo from './../../assets/images/entry-logo.png'
+import SignupButton from './../../assets/images/sign-up-button.png'
+import LoginButton from './../../assets/images/log-in-button.png'
 
 const Entry = ({navigation}) => {
     return (
         <View>
-            <Text
-                style={styles.title}
-                onPress={() => {navigation.navigate('Signup')}}
-            >
-                Signup
-            </Text>
-            <Text
-                style={styles.title}
-                onPress={() => {navigation.navigate('Login')}}
-            >
-                Login
-            </Text>
+            <View style={styles.entryInfoContainer}>
+                <Image
+                    style={styles.entryLogo}
+                    source={EntryLogo}
+                />
+                <View style={styles.entryTextContainer}>
+                    <Text style={styles.entryText}>Take risks.</Text>
+                    <Text style={styles.entryText}>Be fearless.</Text>
+                </View>
+            </View>
+
+            <View style={styles.actions}>
+                <TouchableOpacity onPress={() => {navigation.navigate('Signup')}}>
+                    <Image
+                        source={SignupButton}
+                        style={styles.actionButton}
+                    />
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={() => {navigation.navigate('Login')}}>
+                    <Image
+                        source={LoginButton}
+                        style={styles.actionButton}
+                    />
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    entryInfoContainer: {
+        marginTop: 250,
+        alignItems: 'center',
+        rowGap: 20,
+    },
+
+    entryLogo: {
+
+    },
+
+    entryTextContainer: {
+        rowGap: 3,
+    },
+
+    entryText: {
+        color: '#ffffff',
+        fontSize: 25,
+        textAlign: 'center',
+    },
+
     title: {
         color: '#ffffff',
         fontSize: 30,
-    }
+    },
+
+    actions: {
+        alignItems: 'center',
+        marginTop: 200,
+        rowGap: 20,
+    },
+
+    actionButton: {
+        width: 200,
+        height: 59,
+    },
 })
 
 export default Entry;
