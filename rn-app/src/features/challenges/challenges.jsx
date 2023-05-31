c6b401971db45aa0fd624fcd208bf0abbc4e4d48
sarahchack0 <sarah.m.chacko.23@dartmouth.edu>
2023-05-31 00:56:29 -0400

    create challenge add submit

--- rn-app/src/features/challenges/challenges.jsx
+++ rn-app/src/features/challenges/challenges.jsx
@@ -24,7 +24,6 @@ const Challenges = ({ navigation }) => {
     const [value, setValue] = useState(null);
     const [items, setItems] = useState([
         { label: 'New', value: 'new' },
-        { label: 'Banana', value: 'banana' }
     ]);
 
     const allChallenges = useSelector((state) => state.challenges.allChallenges) || [];
@@ -64,7 +63,6 @@ const Challenges = ({ navigation }) => {
                                 <View style={[styles.trendingBox, index % 2 ? styles.neonRed : styles.neonBlue]} key={challenge.id} >
                                     <Text style={styles.cTitle}> {challenge.title.toUpperCase()} </Text>
                                     <Text style={styles.cExpiry} >{challenge.expiresIn} </Text>
-                                    {/* <Text style={styles.cExpiry} >Expires in {challenge.date.toString()}</Text> */}
                                     <Text style={styles.cDescription} >{challenge.description} </Text>
                                     <Text style={styles.cPoints} >{challenge.points} PTS </Text>
                                 </View>
@@ -111,7 +109,7 @@ const Challenges = ({ navigation }) => {
                                 <View style={styles.allChallengeBox} key={challenge.id} >
                                     <Text style={styles.allTitle}>{challenge.title.toUpperCase()} </Text>
                                     <View style={styles.allRight} >
-                                        <Text style={styles.cExpiry}>{challenge.expiresIn}</Text>
+                                        <Text style={styles.cExpiry}>Expires in {challenge.expiresIn}</Text>
                                         <Text style={styles.aPoints}>{challenge.points} PTS</Text>
                                     </View>
                                 </View>
@@ -301,6 +299,7 @@ const styles = StyleSheet.create({
         display: 'flex',
         flexDirection: 'row',
         alignItems: 'center',
+        paddingHorizontal:5,
 
     },
     allRight: {
